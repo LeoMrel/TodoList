de90760329b323979c8b725cf5fcf2810b014009
Leo <leonardomrel0028@gmail.com>
2022-07-31 22:01:24 +0200

    Working on reset password feature

--- src/components/ResetForm.jsx
+++ src/components/ResetForm.jsx
@@ -1,7 +1,16 @@
+import * as Styles from './Styles/CardStyles'
+import { Link } from 'react-router-dom';
+
 const ResetForm = () => {
 
     return (
-        <div className="text-xl text-red-600">Lolazo</div>
+        <div className={Styles.cardStyles}>
+            <h5  className="text-xl font-medium text-gray-900 dark:text-white">Reset your password</h5>
+            <input className={`mb-5 mt-2 ${Styles.inputStyles}`} placeholder='Email Adress'></input>
+            <Link to='/' className="ml-1 text-sm font-medium text-blue-700 hover:underline dark:text-blue-500">
+                Go back to login
+            </Link>
+        </div>
     )
 }
 
